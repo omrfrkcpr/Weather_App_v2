10d49fc808126a77892e898ff9c3f650f9c25c93
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-02-16 10:40:11 +0100

    add card structure

--- inClass/app.js
+++ inClass/app.js
@@ -1,6 +1,7 @@
 //! Selectors
 const form = document.querySelector("form")
 const input =  document.querySelector("form input")
+const cardContainer = document.getElementById("card-container")
 
 
 
@@ -35,14 +36,35 @@ const getWeatherData = async () => {
 
     try {
         
-        const response = await fetch(url).then((response) => response.json())
+        const response = await fetch(url).then((response) => response.json()) //& fetch ile
 
-        console.log(response) // Api den gelen veri
+        // console.log(response) // Api den gelen veri
+
+        //? Data destructure
+
+        const {main, name, weather, sys} = response //& fetch
+
+        // console.log(weather[0].icon)
+        const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
+
+        let card =
+        `       <div class="col" id="${name}">
+<div class="card mb-4 rounded-3 shadow-sm">
+        <ul class="list-unstyled mt-2 mb-4">
+            <li class="text-end me-2"><i class="bi bi-x-circle"></i></li>
+            <h4 class="my-0 fw-normal">${name} <span ><sup><img src="https://flagsapi.com/${sys.country}/shiny/24.png" class="rounded-circle" alt=${sys.country}/> </sup></span></h4>
+            <h1 class="card-title pricing-card-title"><i class="bi bi-thermometer-half"></i> ${main.temp}<sup>°C</sup></h1>
+            <h6 class="card-title pricing-card-title">Min : ${main.temp_min}<sup>°C</sup> - Max : ${main.temp_max}<sup>°C</sup>  </h6>
+            <h6 class="card-title pricing-card-title"><img src="./assets/wi-barometer.svg" height="30px"/>${main.pressure} <img src="./assets/wi-humidity.svg" height="30px"/>${main.humidity} </h6>
+            <li><img src="${iconUrl}"/></li>
+            <li>${weather[0].description}</li>
+        </ul>
+</div>
+</div>`
 
 
 
-
-
+      cardContainer.innerHTML = card  + cardContainer.innerHTML
 
 
 
